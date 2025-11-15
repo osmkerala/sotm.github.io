eb3cd5cecde654aa5d95e3dda6e3981234a6ba2c
Akshay S Dinesh <akshay@learnlearn.in>
2025-11-15 12:13:57 +0530

    upgrade all dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://osmkerala.github.io",
-  integrations: [react(), tailwind(), mdx()]
+  site: "https://kerala.stateofthemap.in",
+  integrations: [react(), mdx()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
